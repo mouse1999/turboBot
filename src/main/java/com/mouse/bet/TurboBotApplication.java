67d4354ccd7cbd5e184529a9efd640c9905aa97a
Koksi-cyber <68597274+Koksi-cyber@users.noreply.github.com>
2026-01-01 14:47:45 +0100

    first commit

---
+++ src/main/java/com/mouse/bet/TurboBotApplication.java
@@ -0,0 +1,13 @@
+package com.mouse.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mouse/bet/TurboBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mouse.bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
