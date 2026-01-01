90d99ca7f1db9fa288fa14e0c65173bf4033d651
Koksi-cyber <68597274+Koksi-cyber@users.noreply.github.com>
2026-01-01 19:47:47 +0100

    AR1-3 setting up the repo

---
+++ src/main/java/com/mouse/bet/window/MSport.java
@@ -0,0 +1,42 @@
+package com.mouse.bet.window;
+
+import com.mouse.bet.interfaces.BettingWindow;
+
+public class MSport implements BettingWindow, Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean isRunning() {
+        return false;
+    }
+
+    @Override
+    public boolean isPaused() {
+        return false;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
